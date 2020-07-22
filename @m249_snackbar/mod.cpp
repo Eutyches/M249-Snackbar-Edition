81f30420a5ec01fc45be1b89025c88ef5f117107
big-corey <39989971+big-corey@users.noreply.github.com>
2020-07-22 05:44:18 -0600

    M249 Snackbar

---
+++ @m249_snackbar/mod.cpp
@@ -0,0 +1,13 @@
+name = "M249 Snackbar Edition";
+picture = "icon.paa";
+actionName = "M249 Snackbar Edition";
+action = "https://github.com/Eutyches/RHS-Solid-Humvees";
+logo = "icon.paa";
+logoOver = "icon.paa";
+tooltip = "Humvees by Eutyches";
+tooltipOwned = "Eutyches";
+overview = "https://github.com/Eutyches/RHS-Solid-Humvees";
+author = "Eutyches (Corey)";
+overviewPicture = "icon.paa";
+overviewText = "https://github.com/Eutyches/RHS-Solid-Humvees";
+overviewFootnote = "GitHub Repository (GPL-V3)";
