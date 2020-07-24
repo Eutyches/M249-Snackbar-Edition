624d3aee618d15020465f57932895dd0c24a281a
big-corey <39989971+big-corey@users.noreply.github.com>
2020-07-23 19:18:18 -0600

    Update mod.cpp

--- @m249_snackbar/mod.cpp
+++ @m249_snackbar/mod.cpp
@@ -1,13 +1,13 @@
 name = "M249 Snackbar Edition";
 picture = "icon.paa";
 actionName = "M249 Snackbar Edition";
-action = "";
+action = "https://github.com/Eutyches/M249-Snackbar-Edition";
 logo = "icon.paa";
 logoOver = "icon.paa";
 tooltip = "M249 Snackbar Edition";
 tooltipOwned = "Eutyches";
-overview = "";
+overview = "https://github.com/Eutyches/M249-Snackbar-Edition";
 author = "Eutyches";
 overviewPicture = "icon.paa";
-overviewText = "";
-overviewFootnote = "";
+overviewText = "https://github.com/Eutyches/M249-Snackbar-Edition";
+overviewFootnote = "M249 Snackbar Edition GitHub Repository";
